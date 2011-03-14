b3292fef51b655e4785074ed2599c3e12536fb41
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-03-14 16:40:15 +0100

    vlink backup

--- src/nl_vlink.c
+++ src/nl_vlink.c
@@ -2,7 +2,9 @@
  * Lightweight Autonomic Network Architecture
  *
  * LANA vlink control messages via netlink socket. This allows userspace
- * applications like 'vlink' to control the whole LANA vlink layer.
+ * applications like 'vlink' to control the whole LANA vlink layer. Each
+ * vlink type (e.g. Ethernet, Bluetooth, ...) gets its own subsystem with
+ * its operations. Access via a single socket type (NETLINK_VLINK).
  *
  * Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
  * Swiss federal institute of technology (ETH Zurich)
@@ -14,20 +16,90 @@
 
 #include "nl_vlink.h"
 
-static int __init init_nlvlink_module(void)
+static DEFINE_MUTEX(nl_vlink_mutex);
+static struct sock *nl_vlink_sock; /* Fixme! */
+
+void nl_vlink_lock(void)
 {
-	printk(KERN_INFO "LANA netlink vlink layer loaded!\n");
+	mutex_lock(&nl_vlink_mutex);
+}
+EXPORT_SYMBOL_GPL(nl_vlink_lock);
+
+void nl_vlink_unlock(void)
+{
+	mutex_unlock(&nl_vlink_mutex);
+}
+EXPORT_SYMBOL_GPL(nl_vlink_unlock);
+
+int nl_vlink_subsys_register(const struct nl_vlink_subsys *n)
+{
+	nl_vlink_lock();
+	/* Link in */
+	nl_vlink_unlock();
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(nl_vlink_subsys_register);
+
+int nl_vlink_subsys_unregister(const struct nl_vlink_subsys *n)
+{
+	nl_vlink_lock();
+	/* Link out */
+	nl_vlink_unlock();
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(nl_vlink_subsys_unregister);
+
+static int __nl_vlink_rcv(struct sk_buff *skb, struct nlmsghdr *nlh)
+{
+}
+
+static void nl_vlink_rcv(struct sk_buff *skb)
+{
+	nl_vlink_lock();
+	netlink_rcv_skb(skb, &__nl_vlink_rcv);
+	nl_vlink_unlock();
+}
+
+static int __net_init nl_vlink_net_init(struct net *net)
+{
+	nl_vlink_sock = netlink_kernel_create(net, NETLINK_VLINK, 0,
+					      nl_vlink_rcv, NULL, THIS_MODULE);
+	if (!nl_vlink_sock)
+		return -ENOMEM;
 	return 0;
 }
 
-static void __exit cleanup_nlvlink_module(void)
+static void __net_exit nl_vlink_net_exit_batch(struct list_head *net_exit_list)
 {
+	struct net *net;
+
+	list_for_each_entry(net, net_exit_list, exit_list)
+		netlink_kernel_release(net->nfnl_stash);
+}
+
+static struct pernet_operations nl_vlink_net_ops = {
+	.init       = nl_vlink_net_init,
+	.exit_batch = nl_vlink_net_exit_batch,
+};
+
+static int __init init_nl_vlink_module(void)
+{
+	printk(KERN_INFO "LANA netlink vlink layer loaded!\n");
+	return register_pernet_subsys(&nl_vlink_net_ops);
+}
+
+static void __exit cleanup_nl_vlink_module(void)
+{
+	unregister_pernet_subsys(&nl_vlink_net_ops);
 	printk(KERN_INFO "LANA netlink vlink layer removed!\n");
 }
 
-module_init(init_nlvlink_module);
-module_exit(cleanup_nlvlink_module);
+module_init(init_nl_vlink_module);
+module_exit(cleanup_nl_vlink_module);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Daniel Borkmann <dborkma@tik.ee.ethz.ch>");
 MODULE_DESCRIPTION("Netlink subsystem for LANA virtual link layer drivers");
+
--- src/nl_vlink.h
+++ src/nl_vlink.h
@@ -9,6 +9,64 @@
 #ifndef NL_VLINK
 #define NL_VLINK
 
-#define NETLINK_VLINK 0xCAFE /* This is what holds us together! ;-) */
+#ifdef __KERNEL__
 
+#include <linux/types.h>
+#include <linux/rwsem.h>
+
+#define NETLINK_VLINK_RX_OK     0  /* Receive went okay, notify next */
+#define NETLINK_VLINK_RX_BAD    1  /* Receive failed, notify next */
+#define NETLINK_VLINK_RX_EMERG  2  /* Receive failed, do not notify next */
+
+#define NETLINK_VLINK_PRIO_LOW  0  /* Low priority callbacks */
+#define NETLINK_VLINK_PRIO_NORM 1  /* Normal priority callbacks */
+#define NETLINK_VLINK_PRIO_HIGH 2  /* High priority callbacks */
+
+#endif /* __KERNEL__ */
+
+#define NETLINK_VLINK           0xCAFE /* This is what holds us together! */
+
+/* Can be OR'ed from userspace for addressing several subsystems */
+#define VLINK_ETHERNET          1  /* Vlink Ethernet type */
+#define VLINK_BLUETOOTH         2  /* Vlink Bluetooth type */
+#define VLINK_INFINIBAND        4  /* Vlink InfiniBand type */
+#define VLINK_I2C               8  /* Vlink I^2C type */
+
+#ifdef __KERNEL__
+
+struct nl_vlink_callback {
+	int priority;
+	int (*rx)(struct sk_buff *skb, int cmd, struct nlmsghdr *nlh);
+	struct nl_vlink_callback *next;
+};
+
+#define NL_VLINK_CALLBACK_INIT(fct, prio) {			\
+	.rx = (fct),						\
+	.priority = (prio),					\
+	.next = NULL, }
+
+struct nl_vlink_subsys {
+	char *name;
+	u16 type;
+	u8 __id;
+	u8 __count;
+	struct rw_semaphore __rwsem;
+	struct nl_vlink_callback *__head;
+};
+
+#define NL_VLINK_SUBSYS_INIT(varname, sysname, type) {		\
+	.name = (sysname),					\
+	.type = (type),						\
+	.__count = 0,						\
+	.__rwsem = __RWSEM_INITIALIZER((varname).__rwsem),	\
+	.__head = NULL, }
+
+extern void nl_vlink_lock(void);
+extern void nl_vlink_unlock(void);
+
+extern int nl_vlink_subsys_register(const struct nl_vlink_subsys *n);
+extern int nl_vlink_subsys_unregister(const struct nl_vlink_subsys *n);
+
+#endif /* __KERNEL__ */
 #endif /* NL_VLINK */
+
