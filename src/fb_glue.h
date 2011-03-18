114b49d40663ec7ca38fbf89b6334234dd424e3e
Institute TIK <dborkma@pc-10089.(none)>
2011-03-18 18:45:06 +0100

    added fb_glue header

---
+++ src/fb_glue.h
@@ -0,0 +1,30 @@
+/*
+ * Lightweight Autonomic Network Architecture
+ *
+ * Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
+ * Swiss federal institute of technology (ETH Zurich)
+ * Subject to the GPL.
+ */
+
+#ifndef FB_GLUE
+#define FB_GLUE
+
+#include <linux/if.h>
+#include <linux/skbuff.h>
+
+struct functional_block;
+
+struct functional_block_ops {
+	int (*net_fb_rx)(struct functional_block *origin,
+			 struct sk_buff *skb);
+};
+
+struct functional_block {
+	char name[IFNAMSIZ];
+	unsigned int cpu;
+	u16 flags;
+	u16 priv_flags;
+	struct functional_block_ops *ops;
+};
+
+#endif /* FB_GLUE */
