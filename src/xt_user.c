f847d2806e469a96676f8353ad6921909f5b4676
Institute TIK <dborkma@pc-10089.(none)>
2011-03-18 18:31:20 +0100

    adapted makefile, added skeleton for user comm with fbctl

---
+++ src/xt_user.c
@@ -0,0 +1,8 @@
+/*
+ * Lightweight Autonomic Network Architecture
+ *
+ * Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
+ * Swiss federal institute of technology (ETH Zurich)
+ * Subject to the GPL.
+ */
+
