930aa799dbbe3f78931ba2ea8aecd2ef48fc1548
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-04-13 16:38:30 +0200

    added table files

---
+++ src/xt_tables.c
@@ -0,0 +1,23 @@
+/*
+ * Lightweight Autonomic Network Architecture
+ *
+ * Global LANA IDP translation tables, core backend.
+ *
+ * Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
+ * Swiss federal institute of technology (ETH Zurich)
+ * Subject to the GPL.
+ */
+
+#include <linux/module.h>
+
+int init_tables(void)
+{
+	return 0;
+}
+EXPORT_SYMBOL_GPL(init_tables);
+
+void cleanup_tables(void)
+{
+}
+EXPORT_SYMBOL_GPL(cleanup_tables);
+
---
+++ src/xt_tables.h
@@ -0,0 +1,16 @@
+/*
+ * Lightweight Autonomic Network Architecture
+ *
+ * Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
+ * Swiss federal institute of technology (ETH Zurich)
+ * Subject to the GPL.
+ */
+
+#ifndef XT_TABLES
+#define XT_TABLES
+
+extern int init_tables(void);
+extern void cleanup_tables(void);
+
+#endif /* XT_TABLES */
+
