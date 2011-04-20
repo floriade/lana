52f8a25099119e2bb7365a240cb5f3d0646d2e84
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-04-20 19:00:20 +0200

    added dummy files for socket

---
+++ src/xt_socket.c
@@ -0,0 +1,10 @@
+/*
+ * Lightweight Autonomic Network Architecture
+ *
+ * Socket handler for LANA userspace applications.
+ *
+ * Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
+ * Swiss federal institute of technology (ETH Zurich)
+ * Subject to the GPL.
+ */
+
---
+++ src/xt_socket.h
@@ -0,0 +1,13 @@
+/*
+ * Lightweight Autonomic Network Architecture
+ *
+ * Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
+ * Swiss federal institute of technology (ETH Zurich)
+ * Subject to the GPL.
+ */
+
+#ifndef XT_SOCKET_H
+#define XT_SOCKET_H
+
+#endif /* XT_SOCKET_H */
+
