31dd825600c88481322f6a9067d3128453484bc8
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-04-20 17:00:44 +0200

    update, registration and unregistration of fblock

--- src/xt_fblock.h
+++ src/xt_fblock.h
@@ -63,7 +63,7 @@ extern int xchg_fblock(struct fblock *old, struct fblock *new);
 extern struct fblock *search_fblock(idp_t idp);
 extern struct fblock *__search_fblock(idp_t idp);
 
-extern idp_t get_fblock_namespace_mapping(char *name); /* acquires rcu_read_lock */
+extern idp_t get_fblock_namespace_mapping(char *name);
 extern idp_t __get_fblock_namespace_mapping(char *name);
 
 extern int change_fblock_namespace_mapping(char *name, idp_t new); 
@@ -72,31 +72,47 @@ extern int __change_fblock_namespace_mapping(char *name, idp_t new);
 extern int init_fblock_tables(void);
 extern void cleanup_fblock_tables(void);
 
-static inline void init_fblock_notifier_block(struct fblock *fb,
-					      struct notifier_block *nb)
+static inline void init_fblock_subscriber(struct fblock *fb,
+					  struct notifier_block *nb)
 {
 	nb->priority = 0;
 	nb->notifier_call = fb->ops->event_rx;
 	nb->next = NULL;
 }
 
-static inline int call_fblock_subscribers(struct fblock *fb,
-					  unsigned long cmd, void *arg)
+static inline int
+fblock_register_foreign_subscriber(struct fblock *us,
+				   struct notifier_block *remote)
 {
-	if (unlikely(!fb->others))
+	return atomic_notifier_chain_register(&us->others->subscribers,
+					      remote);
+}
+
+static inline void
+fblock_unregister_foreign_subscriber(struct fblock *us,
+				     struct notifier_block *remote)
+{
+	atomic_notifier_chain_unregister(&us->others->subscribers, remote);
+}
+
+static inline int notify_fblock_subscribers(struct fblock *us,
+					    unsigned long cmd, void *arg)
+{
+	if (unlikely(!us->others))
 		return -ENOENT;
-	return atomic_notifier_call_chain(&fb->others->subscribers, cmd, arg);
+	return atomic_notifier_call_chain(&us->others->subscribers, cmd, arg);
 }
 
-static inline void get_fblock(struct fblock *b)
+static inline void get_fblock(struct fblock *fb)
 {
-	atomic_inc(&b->refcnt);
+	atomic_inc(&fb->refcnt);
 }
 
-static inline void put_fblock(struct fblock *b)
+static inline void put_fblock(struct fblock *fb)
 {
-	if (likely(!atomic_dec_and_test(&b->refcnt)))
+	if (likely(!atomic_dec_and_test(&fb->refcnt)))
 		return;
-	kfree_fblock(b);
+	kfree_fblock(fb);
 }
+
 #endif /* XT_FBLOCK_H */
