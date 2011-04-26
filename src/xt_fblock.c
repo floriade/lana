027f5b14b8bb03da05ffb8f9c46c4b60ed0e558e
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-04-26 17:37:54 +0200

    update on formatting

--- src/xt_fblock.c
+++ src/xt_fblock.c
@@ -30,11 +30,14 @@ struct idp_elem {
 	struct rcu_head rcu;
 } ____cacheline_aligned;
 
-static struct critbit_tree idpmap; /* string -> idp translation map */
+static struct critbit_tree idpmap;         /* string -> idp translation map */
 static struct fblock **fblmap_head = NULL; /* idp -> fblock translation map */
 static spinlock_t fblmap_head_lock;
+
 static atomic64_t idp_counter;
+
 static struct kmem_cache *fblock_cache = NULL;
+
 extern struct proc_dir_entry *lana_proc_dir;
 static struct proc_dir_entry *fblocks_proc;
 
