3f3f5429aeb12b849027abe03dfc567b28360d72
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-04-28 14:47:44 +0200

    added netlink userctl kernel group

--- src/xt_user.c
+++ src/xt_user.c
@@ -1,10 +1,59 @@
 /*
  * Lightweight Autonomic Network Architecture
  *
- * LANA NETLINK handler for user requests coming from fbctl.
+ * LANA NETLINK handler for Functional Block userspace control.
  *
  * Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
  * Swiss federal institute of technology (ETH Zurich)
  * Subject to the GPL.
  */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/socket.h>
+#include <linux/net.h>
+#include <linux/skbuff.h>
+#include <net/netlink.h>
+#include <net/sock.h>
+
+#include "xt_user.h"
+
+static struct sock *userctl_sock = NULL;
+
+static int __userctl_rcv(struct sk_buff *skb, struct nlmsghdr *nlh)
+{
+	struct lananlmsg *lmsg;
+
+	if (security_netlink_recv(skb, CAP_NET_ADMIN))
+		return -EPERM;
+	if (nlh->nlmsg_len < NLMSG_LENGTH(sizeof(struct lananlmsg)))
+		return 0;
+
+	lmsg = NLMSG_DATA(nlh);
+	printk(KERN_INFO "[lana] lananlmsg received: %u, %s!\n",
+	       lmsg->cmd, lmsg->buff);
+
+	return 0;
+}
+
+static void userctl_rcv(struct sk_buff *skb)
+{
+	netlink_rcv_skb(skb, &__userctl_rcv);
+}
+
+int init_userctl_system(void)
+{
+	userctl_sock = netlink_kernel_create(&init_net, NETLINK_USERCTL,
+					     USERCTLGRP_MAX, userctl_rcv,
+					     NULL, THIS_MODULE);
+	if (!userctl_sock)
+		return -ENOMEM;
+	return 0;
+}
+
+void cleanup_userctl_system(void)
+{
+	netlink_kernel_release(userctl_sock);
+}
 
--- src/xt_user.h
+++ src/xt_user.h
@@ -9,5 +9,26 @@
 #ifndef XT_USER_H
 #define XT_USER_H
 
+#include <linux/types.h>
+
+#include "xt_vlink.h"
+
+#define NETLINK_USERCTL 24
+
+enum userctl_groups {
+	USERCTLGRP_NONE = VLINKNLGRP_MAX, /* Reserved                    */
+#define USERCTLGRP_NONE		USERCTLGRP_NONE
+	USERCTLGRP_CONF,
+#define USERCTLGRP_CONF		USERCTLGRP_CONF
+	 __USERCTLGRP_MAX
+};
+
+#define USERCTLGRP_MAX		(__USERCTLGRP_MAX - 1)
+
+struct lananlmsg {
+	u32 cmd;
+	char buff[1500];
+};
+
 #endif /* XT_USER_H */
 
