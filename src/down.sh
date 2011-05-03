5622c37ca2a6dac78a04f9c05ed958e03007b082
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-05-03 17:49:27 +0200

    added up and down scripts for testing

---
+++ src/down.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+rmmod testskb
+
+../usr/fbctl unbind fb2 fb3
+../usr/fbctl unbind fb1 fb2
+../usr/fbctl rm fb3
+../usr/fbctl rm fb2
+../usr/fbctl rm fb1
+
+echo "-1" > /proc/net/lana/ppesched
+
+sleep 1
+
+rmmod fb_dummy
+rmmod sd_single
+rmmod lana
+
+echo "+++ done +++"
+
---
+++ src/up.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+insmod lana.ko
+insmod sd_single.ko
+insmod fb_dummy.ko
+
+../usr/fbctl add fb1 dummy
+../usr/fbctl add fb2 dummy
+../usr/fbctl add fb3 dummy
+../usr/fbctl bind fb1 fb2
+../usr/fbctl bind fb2 fb3
+
+insmod testskb.ko
+
+echo "+++ done +++"
+
