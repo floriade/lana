7c565afecba136008791348fcb3a39e3a6e0a045
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-05-17 12:06:20 +0200

    added conf file

---
+++ src/xt_conf.h
@@ -0,0 +1,16 @@
+/*
+ * Lightweight Autonomic Network Architecture
+ *
+ * Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
+ * Swiss federal institute of technology (ETH Zurich)
+ * Subject to the GPL.
+ */
+
+#ifndef XT_CONF_H
+#define XT_CONF_H
+
+#define __HIGHPERF 1
+#define USERSPACECPU 0
+
+#endif /* XT_CONF_H */
+
