c0006ce91a705ef27d64e560abb24a6b86a4b37f
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-05-31 16:12:43 +0200

    simplified testapp

--- opt/pflana.c
+++ opt/pflana.c
@@ -9,41 +9,19 @@
 
 #define AF_LANA		27
 
-static sig_atomic_t sigint = 0;
-
-static void intr(int sig)
-{
-	sigint = 1;
-}
-
-static inline void register_signal(int signal, void (*handler)(int))
-{
-	sigset_t block_mask;
-	struct sigaction saction;
-
-	sigfillset(&block_mask);
-	saction.sa_handler = handler;
-	saction.sa_mask = block_mask;
-	saction.sa_flags = SA_RESTART;
-
-	sigaction(signal, &saction, NULL);
-}
-
 int main(void)
 {
 	int sock, i;
 	char buff[256];
 
-	register_signal(SIGINT, intr);
 	sock = socket(AF_LANA, SOCK_DGRAM, 0);
 	if (sock < 0) {
 		perror("socket");
 		return 0;
 	}
 
-/*
 	printf("Worked! Abort with ^C\n");
-	while (!sigint) {
+	while (1) {
 		memset(buff, 0, sizeof(buff));
 		int ret = recv(sock, buff, sizeof(buff), 0);
 		if (ret < 0) {
@@ -57,7 +35,6 @@ int main(void)
 			printf("\n\n");
 		}
 	}
-*/
 
 	close(sock);
 	return 0;
