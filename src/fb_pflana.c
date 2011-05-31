1ef7a7d260c2cd61675316866336baeb9a9add01
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-05-31 20:20:23 +0200

    update

--- src/fb_pflana.c
+++ src/fb_pflana.c
@@ -204,19 +204,9 @@ static int lana_proto_recvmsg(struct kiocb *iocb, struct sock *sk,
 
 static int lana_proto_backlog_rcv(struct sock *sk, struct sk_buff *skb)
 {
-	int err = 0;
-	if (unlikely(!sk)) {
+	int err = sock_queue_rcv_skb(sk, skb);
+	if (err != 0)
 		kfree_skb(skb);
-		return NET_RX_DROP;
-	}
-	if (atomic_read(&sk->sk_rmem_alloc) >=
-	    sk->sk_rcvbuf) {
-		kfree_skb(skb);
-	} else {
-		err = sock_queue_rcv_skb(sk, skb);
-		if (err != 0)
-			kfree_skb(skb);
-	}
 	return err ? NET_RX_DROP : NET_RX_SUCCESS;
 }
 
