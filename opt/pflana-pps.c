b479c6ad5a1e6ba191749be92a89337660f11b29
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-06-06 18:28:01 +0200

    userspace test prog

---
+++ opt/pflana-pps.c
@@ -0,0 +1,79 @@
+/* Compile with -lrt */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define AF_LANA		27
+
+static sig_atomic_t sigint = 0;
+
+static inline double timespec_to_double(const struct timespec *time)
+{
+	return time->tv_nsec * 1E-9 + (double) time->tv_sec;
+}
+
+static void sig_handler(int sig)
+{
+	if (sig == SIGINT)
+		sigint = 1;
+}
+
+int main(void)
+{
+	int sock, ret;
+	char buff[1600];
+        struct timespec before, after;
+	unsigned long long pkts = 0, byte = 0;
+	double x1, x2, elapsed;
+
+	if (geteuid() != 0) {
+		fprintf(stderr, "Not root?!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	signal(SIGINT, sig_handler);
+
+	sock = socket(AF_LANA, SOCK_RAW, 0);
+	if (sock < 0) {
+		perror("socket");
+		return 0;
+	}
+
+	printf("Worked! Abort with ^C\n");
+
+	memset(&before, 0, sizeof(before));
+	memset(&after, 0, sizeof(after));
+
+        clock_gettime(CLOCK_REALTIME, &before);
+	while (!sigint) {
+		ret = recv(sock, buff, sizeof(buff), 0);
+		if (ret < 0) {
+			perror("recvmsg");
+			continue;
+		} else {
+			pkts++;
+			byte += ret;
+		}
+	}
+        clock_gettime(CLOCK_REALTIME, &after);
+
+	x1 = timespec_to_double(&after);
+	x2 = timespec_to_double(&before);
+	elapsed = x1 - x2;
+
+	printf("time: %lf, pkts/s: %.2lf, bytes/s: %.2lf\n",
+	       elapsed, 1.0 * pkts / elapsed, 1.0 * byte / elapsed);
+
+	close(sock);
+	return 0;
+}
+
